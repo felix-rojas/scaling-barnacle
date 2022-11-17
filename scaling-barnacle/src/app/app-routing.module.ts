7c5581167853668202e1d6506af66eb632e70784
felix-rojas <A01201946@tec.mx>
2022-11-17 16:24:43 -0600

    move pokemon component to its own route

--- scaling-barnacle/src/app/app-routing.module.ts
+++ scaling-barnacle/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+//dependeencies
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonComponent } from './pokemon/pokemon.component';
 
-const routes: Routes = [];
+// more specific routes should be placed above less specific routes
+// define the pokemon route to use
+const routes: Routes = [
+{ path: 'pokemon-component', component: PokemonComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
